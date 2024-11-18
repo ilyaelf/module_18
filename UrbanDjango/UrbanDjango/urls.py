31dfa29bff39ad14eee0ff68bba19a1b5be9731d
ilyaelf <bulletdodger@yandex.ru>
2024-11-18 17:04:38 +0300

    18_4

--- UrbanDjango/UrbanDjango/urls.py
+++ UrbanDjango/UrbanDjango/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from task2.views import func_view, ClassView
-from task3.views import platform,products,info
+from task4.views import platform,products,info
 from django.views.generic import TemplateView
 
 urlpatterns = [
